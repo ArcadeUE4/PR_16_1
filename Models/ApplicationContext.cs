904292c235c5d512355c589b2c9612851932f083
civilmob1234 <raiden123926@gmail.com>
2022-05-03 23:45:48 +0300

    Добавьте файлы проекта.

---
+++ Models/ApplicationContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace PR_16_1.Models
+{
+    public class ApplicationContext : DbContext
+    {
+        public DbSet<Friend> Friends { get; set; }
+        public ApplicationContext(DbContextOptions<ApplicationContext> options)
+            : base(options)
+        {
+            Database.EnsureCreated();
+        }
+
+    }
+}
